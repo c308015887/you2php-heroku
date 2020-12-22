b6ed570711fba0e8e8314f5ac4da530d9f96cf6e
c308015887 <35992064+c308015887@users.noreply.github.com>
2020-12-22 21:37:39 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAq2j3ZjCqLqC1Ur03bOoueU-M_B1h66N8');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
